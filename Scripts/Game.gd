3edd127e30a78812ec8728a2fa2d2b3e6d211b0b
RisingThumb <aaronleonard@risingthumb.xyz>
2021-06-12 08:00:05 +0100

    Animations between the parts

--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -2,6 +2,32 @@ extends Control
 
 
 var day = 1
+var money = 30
 
 func _ready():
-	pass # Replace with function body.
+	$DayPanel.visible = true
+	$AgencyPanel.visible = false
+	$NewspaperCreationPanel.visible = false
+	$BillPanel.visible = false
+	
+	$Fader.play("DayPanelFadeOut")
+
+func dayForward():
+	day+=1
+	$DayPanel/CenterContainer/DayLabel.updateDayText(day)
+
+func setupAgenciesAvailable():
+	$AgencyPanel/CenterContainer/OptionButton.items.clear()
+	$AgencyPanel/CenterContainer/OptionButton.add_item("Select an Agency to work for")
+	$AgencyPanel/CenterContainer/OptionButton.selected = 0
+	$AgencyPanel/CenterContainer/OptionButton.add_item("Agency day 1")
+
+func setupTopicDropDowns():
+	# Gets called when the agency is selected.
+	pass
+
+
+func _on_OptionButton_item_selected(index):
+	print(index)
+	$AgencyPanel/CenterContainer/OptionButton.disabled = true
+	$Fader.play("AgencyPanelFadeOut")
