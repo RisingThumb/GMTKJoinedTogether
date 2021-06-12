5191e6d7dbe1a2bfdac3999f5d04b39f1290cb06
Cam White (UG) <b8003560@newcastle.ac.uk>
2021-06-12 19:25:54 +0100

    Bic

--- Scripts/PathTree.gd
+++ Scripts/PathTree.gd
@@ -1,16 +1,59 @@
 extends Node
 
+export(String) var path_tree_file
+var root_path_tree_state
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+class State:
+	var name: String
+	var description: String
+	
+	var children: Array
+	var thresholds: Array
+	
+	func get_name():
+		return name
+		
+	func get_description():
+		return description
+		
+	func get_children():
+		return children
+		
+	func get_thresholds():
+		return thresholds
+		
+	func _set_children(_children: Array):
+		children = _children
+		
+	func _set_thresholds(_thresholds: Array):
+		thresholds = _thresholds
+		
+func _construct_tree(data):
+	var states = []
+	
+	for object in data["states"]:
+		states.push_back(object["name"], object["description"])
+	
+	for association in data["associations"]:
+		var to = association["to"]
+		var from = association["from"]
+		
+		var children = states[from].get_children()
+		children.push_back(states[to])
+	
+	return states[0]
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	var document = _load_file(path_tree_file)
+	var parsed = JSON.parse(document)
+	
+	root_path_tree_state = _construct_tree(parsed.result)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _load_file(name):
+	var file = File.new()
+	file.open(name, File.READ)
+	
+	var content = file.get_as_text()
+	file.close()
+	
+	return content
